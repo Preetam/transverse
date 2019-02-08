66929e718f65c4844c3bd6a88cf734c405832fcb
Preetam Jinka <pj@preet.am>
2019-02-08 08:19:13 -0800

    update deps

---
+++ web/webpack.dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "mode": "development",
+    "entry": __dirname+"/js/index.js",
+    "output": {
+        "path": __dirname+'/static/js',
+        "filename": "app.min.js"
+    }
+}
---
+++ web/webpack.prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "mode": "production",
+    "entry": __dirname+"/js/index.js",
+    "output": {
+        "path": __dirname+'/static/js',
+        "filename": "app.min.js"
+    }
+}
