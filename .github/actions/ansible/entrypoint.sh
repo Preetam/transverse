9ba19b7306241489366a2a6df0881b7b88b9980b
Preetam Jinka <pj@preet.am>
2019-09-21 18:10:16 -0700

    try out GitHub Actions

---
+++ .github/actions/ansible/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "$VAULT_PASS" > ~/.vault_pass.txt
+mkdir ~/.ssh
+ansible-vault --vault-password-file ~/.vault_pass.txt view ansible/ssh_key.txt.secret > ~/.ssh/id_rsa
+chmod 0600 ~/.ssh/id_rsa
+
+ansible-playbook --vault-password-file ~/.vault_pass.txt -i ansible/hosts ansible/site.yml
